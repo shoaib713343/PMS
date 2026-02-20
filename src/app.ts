174f0e3b244f4e230255b8912b5534b262e95543
Shoaib <shoaib713343@gmail.com>
2026-02-20 15:15:06 +0530

    fixed cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { errorHandler } from "./middleware/errorHandler";
 import authRouter from "./features/auth/auth.routes";
 // import projectRouter from "./features/projects/project.routes"
 import path from "path";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",  
+      "http://localhost:5173", 
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
