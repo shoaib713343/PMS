9bb9b69c1ac947a1fb9c469880ce7e447cf66704
Shoaib <shoaib713343@gmail.com>
2026-04-20 18:20:22 +0530

    removed validation om deleting user

--- src/features/users/user.routes.ts
+++ src/features/users/user.routes.ts
@@ -13,6 +13,6 @@ router.post("/",protect, allowRoles("super_admin"), validate(createUserSchema),a
 router.get("/", protect, allowRoles("admin", "super_admin"), asyncHandler(listUsersController));
 router.get("/:id", protect, allowRoles("admin", "super_admin"), asyncHandler(getUserController));
 router.put("/:id", protect, allowRoles("super_admin"), asyncHandler(updateUserController));
-router.delete("/:id", protect, allowRoles("super_admin"), validate(updateUserSchema), asyncHandler(deleteUserController));
+router.delete("/:id", protect, allowRoles("super_admin"), asyncHandler(deleteUserController));
 
 export default router;
